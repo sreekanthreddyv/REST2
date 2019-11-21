a9e087d0dad15e23ecc3171816ba2b9dd48fa15a
Sreekanth Reddy V (Temp) <c_srev@qti.qualcomm.com>
2019-11-21 11:03:29 +0530

    DB_notes

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,8 @@ from .models import Poll
 
 # Create your views here.
 def polls_list(request):
-	MAX_OBJECTS = 20
-	polls = Poll.objects.all()[:MAX_OBJECTS]
+	max_objects = 20
+	polls = Poll.objects.all()[:max_objects]
 	data = {"results": list(polls.values("question", "created_by__username", "pub_date"))}
 	return JsonResponse(data)
 
